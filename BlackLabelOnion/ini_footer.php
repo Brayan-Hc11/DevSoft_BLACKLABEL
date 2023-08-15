13108f94d9f4270cdd6916a767472feb745fdc37
Brayan Andres Hc <118775234+Brayan-Hc11@users.noreply.github.com>
2023-08-14 20:50:35 -0500

    Add files via upload

---
+++ BlackLabelOnion/ini_footer.php
@@ -0,0 +1,37 @@
+<!--LINKIAMOS LAS HOJAS DE ESTILO-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+<link rel="stylesheet" href="main.css">
+
+<?php
+Class inifooter{
+    public function vistaFooter(){
+echo("
+<footer class='footer'>
+
+            <div class='footer__content container'>
+                <div class='footer_link'>
+
+                    <table class='footer__tabla'>
+                        <th><a href='#'><img src='img_facebook.jpg' height='26px' width='29px'></a></th> 
+                        <th><a href='#'><img src='img_instagram.jpg' height='35px' width='47px'></a></th>
+                        <th><a href='#'><img src='img_twitter.jpg' height='22px' width='31px'></a></th>
+                    </table>
+                </div>
+
+                <div class='footer__link'>
+                    <h3><a href='blackLabel.html'><span class='material-symbols-outlined'>sell</span> BlackLabel</h3></a>
+                </div>
+
+                <div class='footer__link'>
+                    <h3>&copy; 2023 DevSoft.</h3>
+                </div>
+
+            </div>
+        </footer>
+");
+    }
+}
+#Finalizamos clase y metodo
+$final = new inifooter();
+    $final ->vistaFooter();
+?>
