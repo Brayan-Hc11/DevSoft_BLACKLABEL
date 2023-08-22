6a4a20556966527b68e03d2ed0af0a9dc4f1d63b
Brayan Andres Hc <118775234+Brayan-Hc11@users.noreply.github.com>
2023-08-21 20:54:55 -0500

    Add files via upload

---
+++ BlackLabelPrueba/Cpa_mid-Categorias_html.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script src="https://kit.fontawesome.com/b217920474.js" crossorigin="anonymous"></script>
+    <script src='jquery-3.2.1.js'></script>
+    <script src="script.js"></script>
+    
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />    
+    <link rel="stylesheet" href="main.css">
+    
+    <title>Categorias</title>
+</head>
+
+<body>
+<div class="Principal">
+<?php
+    include('Cpa_ini-Categorias--header.php')
+    ?>
+   <div class="wrap">
+    <h1></h1>
+    <div class="wrap__store-wrapper">
+        <div class="wrap__store-wrapper--category_list">
+            <a href="#" class="wrap__store-wrapper--category_list--Item " category="All">Todo</a>
+            <a href="#" class="wrap__store-wrapper--category_list--Item" category="Ordenadores">Ordenadores</a>
+            <a href="#" class="wrap__store-wrapper--category_list--Item" category="Laptos">Laptos</a>
+            <a href="#" class="wrap__store-wrapper--category_list--Item" category="Smartphones">Smartphones</a>
+            <a href="#" class="wrap__store-wrapper--category_list--Item" category="Monitores">Monitores</a>
+            <a href="#" class="wrap__store-wrapper--category_list--Item" category="Audifonos">Audifonos</a>
+            <a href="#" class="wrap__store-wrapper--category_list--Item" category="Ordenadores">Ordenadores</a>
+            <a href="#" class="wrap__store-wrapper--category_list--Item" category="Laptos">Laptos</a>
+            <a href="#" class="wrap__store-wrapper--category_list--Item" category="Smartphones">Smartphones</a>
+            <a href="#" class="wrap__store-wrapper--category_list--Item" category="Monitores">Monitores</a>
+            <a href="#" class="wrap__store-wrapper--category_list--Item" category="Audifonos">Audifonos</a>
+        </div>
+
+<!--
+        cambio de clases
+        contenedor=almacen
+        contenedor-items =almacen-items
+        item= elment
+        titulo-item =titulo-element
+        img-item = img-element
+        precio-item = precio-element
+        boton-item = boton-element 
+
+<section class="almacen">
+    <div class="almacen-elements">
+        
+    <div class="element">
+            <span class="titulo-element">box engasse</span>
+                <img src="./img/boxengasse.png" alt="" class="img-element">
+                <span class="precio-element">$15.000</span>
+            <button class="boton-element">agregar</button>
+    </div>
+    <div class="element">
+            <span class="titulo-element">English horse</span>
+                <img src="./img/englishrose.png" alt="" class="img-element">
+                <span class="precio-element">$15.000</span>
+            <button class="boton-element">agregar</button>
+    </div>
+    <div class="element">
+            <span class="titulo-element">Knokc nap</span>
+                <img src="./img/knocknap.png" alt="" class="img-element">
+                <span class="precio-element">$15.000</span>
+            <button class="boton-element">agregar</button>
+        </div>
+        <div class="element">
+            <span class="titulo-element">la night</span>
+                <img src="./img/lanight.png" alt="" class="img-element">
+                <span class="precio-element">$15.000</span>
+            <button class="boton-element">agregar</button>
+        </div>
+        <div class="element">
+            <span class="titulo-element">silver</span>
+                <img src="./img/silverall.png" alt="" class="img-element">
+                <span class="precio-element">$15.000</span>
+            <button class="boton-element">agregar</button>
+        </div>
+        <div class="element">
+            <span class="titulo-element">skin glam</span>
+                <img src="./img/skinglam.png" alt="" class="img-element">
+                <span class="precio-element">$15.000</span>
+            <button class="boton-element">agregar</button>
+        </div>
+        <div class="element">
+            <span class="titulo-element">mid mix </span>
+                <img src="./img/middlesteel.png" alt="" class="img-element">
+                <span class="precio-element">$15.000</span>
+            <button class="boton-element">agregar</button>
+        </div>
+        <div class="element">
+            <span class="titulo-element">sir blue</span>
+                <img src="./img/sirblue.png" alt="" class="img-element">
+                <span class="precio-element">$15.000</span>
+            <button class="boton-element">agregar</button>
+        </div>
+        <div class="element">
+            <span class="titulo-element">middlesteel</span>
+                <img src="./img/middlesteel.png" alt="" class="img-element">
+                <span class="precio-element">$15.000</span>
+            <button class="boton-element">agregar</button>
+        </div>
+        <div class="element">
+            <span class="titulo-element">box engasse</span>
+                <img src="./img/boxengasse.png" alt="" class="img-element">
+                <span class="precio-element">$15.000</span>
+            <button class="boton-element">agregar</button>
+        </div>
+    </div>
+
+    <div class="carrito">
+        <div class="header-carrito">
+            <h2>tu carrito</h2>
+        </div>
+
+        <div class="carrito-items">
+            <div class="carrito-item">
+                <img src="./img/boxengasse.png" alt="" width="80px">
+                <div class="carrritt-item-detalles">
+                    <span class="carrito-item-titulo">box engasse</span>
+                    <div class="selector-cantidad">
+                        <i class="fa-solid fa-minus restar-cantidad"></i>
+                            <input type="text" value="1" class="carrito-item-cantidad" disabled="disabled">
+                        <i class="fa-solid fa-plus sumar-cantidad"></i>                      
+                    </div>
+                    <span class="carrito-item-precio">$15.000</span>
+                </div>
+                <span class="btn-eliminar">
+                    <i class="fa-solid fa-trash"></i>
+                </span>
+            </div>
+
+            <div class="carrito-item">
+                <img src="./img/englishrose.png" alt="" width="80px">
+                <div class="carrritt-item-detalles">
+                    <span class="carrito-item-titulo">englishrose</span>
+                    <div class="selector-cantidad">
+                        <i class="fa-solid fa-minus restar-cantidad"></i>
+                            <input type="text" value="2" class="carrito-item-cantidad" disabled="disabled">
+                        <i class="fa-solid fa-plus sumar-cantidad"></i>                      
+                    </div>
+                    <span class="carrito-item-precio">$30.000</span>
+                </div>
+                <span class="btn-eliminar">
+                    <i class="fa-solid fa-trash"></i>
+                </span>
+            </div>
+        </div>
+        <div class="carrito-total">
+            <div class="fila">
+                <strong>tu total </strong>
+                <span class="carrito-precio-total">$120.000</span>
+            </div>
+            <button class="btn-pagar">pagar <i class="fa-solid fa-bag-shopping"></i> </button>
+        </div>
+    </div>
+</section>
+-->
+<section class="wrap__store-wrapper--products-list">
+
+            <div class="wrap__store-wrapper--products-list--item" category="Laptos">
+                <img src="images/laptop_hp.jpg" alt="">
+                <a href="#">Laptop hp</a>
+            </div>
+
+            <div class="wrap__store-wrapper--products-list--item" category="Laptos">
+                <img src="images/laptop_toshiba.jpg" alt="">
+                <a href="#">Laptop Thosiba</a>
+            </div>
+
+            <div class="wrap__store-wrapper--products-list--item" category="Smartphones">
+                <img src="images/samsung_galaxy.jpg" alt="">
+                <a href="#">samsung galaxy</a>
+            </div>
+            
+            <div class="wrap__store-wrapper--products-list--item" category="Smartphones">
+                <img src="images/iphone.jpg" alt="">
+                <a href="#">iphone</a>
+            </div>
+            
+            <div class="wrap__store-wrapper--products-list--item" category="Ordenadores">
+                <img src="images/pc_hp.jpg" alt="">
+                <a href="#">pc hp</a>
+            </div>
+
+            <div class="wrap__store-wrapper--products-list--item" category="Ordenadores">
+                <img src="images/pc_lenovo.jpg" alt="">
+                <a href="#">pc lenovo</a>
+            </div>
+            
+            <div class="wrap__store-wrapper--products-list--item" category="Monitores">
+                <img src="images/monitor_asus.jpg" alt="">
+                <a href="#">monitor asus</a>
+            </div>
+
+            <div class="wrap__store-wrapper--products-list--item" category="Audifonos">
+                <img src="images/jbl.jpg" alt="">
+                <a href="#">Audofonos jbl</a>
+            </div>
+        </section>
+   
+    </div>
+    <h1></h1>
+</div>
+</div>
+</body>
+<?php include("Cpa_fin-productos--footer.php")?>
+</html>
