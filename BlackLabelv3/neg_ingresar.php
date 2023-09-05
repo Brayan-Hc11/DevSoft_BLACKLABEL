d90f65c3886e0bc689b9117b83338b4baf86033d
Brayan Andres Hc <118775234+Brayan-Hc11@users.noreply.github.com>
2023-09-05 15:39:23 -0500

    Add files via upload

--- BlackLabelv3/neg_ingresar.php
+++ BlackLabelv3/neg_ingresar.php
@@ -1,53 +1,29 @@
 <?php
-
-  session_start();
-
-class usuario
-{
-
-    public function ingresar($correo,$contrasena)
-    {
-
+session_start();
+class usuario{
+   public function ingresar($correo,$contrasena){
         include("conexion.php");
+            $cont = "0";//creo un contador y lo inicializo en 0
+            $sql = "SELECT * FROM usuarios WHERE correo='$correo'";//hago el select from desde la tabla con el documento
 
-        $cont = "0";//creo un contador y lo inicializo en 0
-
-        $sql = "SELECT * FROM usuarios WHERE correo='$correo'";//hago el select from desde la tabla con el documento
-
-        if (!$result = $db->query($sql))
-        {
+        if (!$result = $db->query($sql)){
             die('Hay un error corriendo en la consulta o los datos no se encontraron!!! [' . $db->error . ']');
-        }
-
-        while ($row = $result->fetch_assoc())
-        {
+        }while ($row = $result->fetch_assoc()){
             $ccontraseña = stripslashes($row["contraseña"]); //codigo que trae los campos de la tabla
             $ddocumento = stripslashes($row["documento"]); 
         }
 
-        if (password_verify($contrasena, $ccontraseña))
-        {
+        if (password_verify($contrasena, $ccontraseña)){
             $cont = "1";
-        }
-
-        if ($cont == "0")
-        {
-
+        }if ($cont == "0"){
             echo "Usted no se encuentra registrado";
-
-        }else
-        {
-
+        }else{
             echo "Ha ingresado correctamente";
             $_SESSION["documento"]=$ddocumento;
             header("Location: vista_permisos.php");
-
         }
-        
     }
-
 }
-
 $final = new usuario();
-$final->ingresar($_POST["correo"],$_POST["contrasena"]);
+    $final->ingresar($_POST["correo"],$_POST["contrasena"]);
 ?>
