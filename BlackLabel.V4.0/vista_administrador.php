f25842011efedc083de1c5ad392f2684a35f92e6
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-09-20 08:35:31 -0500

    Update vista_administrador.php

--- BlackLabel.V4.0/vista_administrador.php
+++ BlackLabel.V4.0/vista_administrador.php
@@ -14,7 +14,7 @@
 
             $ccorreo="";//inicializo las variables
 
-            $ddocumento = $_SESSION["documento"];//
+            $ddocumento = $_SESSION["documento"];//sesion 
 
             //consulta 
             $sql = "SELECT * FROM usuarios WHERE documento ='$ddocumento'";
