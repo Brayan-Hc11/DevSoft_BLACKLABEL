ff12c2d663cb6620b9e7c26692d84604e1c0899a
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-09-20 08:36:24 -0500

    Update vista_administrador.php

--- BlackLabel.V4.0/vista_administrador.php
+++ BlackLabel.V4.0/vista_administrador.php
@@ -14,7 +14,7 @@
 
             $ccorreo="";//inicializo las variables
 
-            $ddocumento = $_SESSION["documento"];//sesion del documento
+            $ddocumento = $_SESSION["documento"];//sesion del documento, donde se guarda en la variable 
 
             //consulta 
             $sql = "SELECT * FROM usuarios WHERE documento ='$ddocumento'";
