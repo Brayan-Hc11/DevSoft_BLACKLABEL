218208125bc8a881d4136c7feef49a2b61f0b063
Brayan Andres Hc <118775234+Brayan-Hc11@users.noreply.github.com>
2023-10-10 17:30:39 -0500

    Update vista_permisos.php

--- BlackLabel.V4.0/vista_permisos.php
+++ BlackLabel.V4.0/vista_permisos.php
@@ -1,150 +1,115 @@
-
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+<title>Selecciona tu Rol</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
 <style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing:border-box ;
+    font-family: sans-serif;
+}
 body{
-
-    background-image: url("");
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    background-image: url("img_fondo_blacklabel-min.jpg");
     background-image: cover ;
     background-position: center;
     background-repeat: no-repeat;
-
 }
-
-
-.title{
-
-    font: caption;
-    font-size: 25px;
-    padding: 30px;
-    text-align: center;
-}
-
 a{
     text-decoration: none;
-    color: #000;
+    color: black;
 }
 
-div{
-
-    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
-    /*background-color: #F1F1F1;*/
-    width: 150px;
-    font: caption;
-    border-radius: 15px;
+/*Estilos iniciales */
+.Roles{
+padding: 2%;
+}
+.Roles:hover{
+    cursor: pointer;
+    transform: translateY(-10px);
+    transition: all 1s ease;
+}
+.card{
+    position: relative;
+    width: 250px;
+    height: 350px;
+    border-radius: 10px;
+    box-shadow: 2px 3px 5px rgba(73, 69, 52, 0.4);
+    background-color: white;
+}
+.card .content{
+    position: relative;
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
     overflow: hidden;
-
+    text-align: center;
+    padding: 20px;
+    background:white;
 }
-
-div:before, div:after {
-
-  content: "";
-  z-index: -1;
-  position: absolute;
-  width: 50%;
-  height: 100%;
-  top: 0;
-  left: -50%;
-  background-color: #ECECEC; /* color de fondo hover */
-  -webkit-transition: all 0.3s ease-in-out;
-  -o-transition: all 0.3s ease-in-out;
-  transition: all 0.3s ease-in-out;
-
+.card .content .img img{
+    position: relative;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    object-fit: cover;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.8);
 }
-
-div:after {
-
-  left: 100%;
-
+.card .content .fondo{
+    position: absolute;
+    width: 350px;
+    height: 200px;
+    background: linear-gradient(to right, #085078, #0c7ab6);
+    transform: rotate(-35deg);
+    top: -50px;
+    left: -100px;
 }
-
-div:hover {
-
-  color: #fff; /* color de fuente hover */
-
+.card .content h4{
+    text-transform: uppercase;
+    font-size: 50px;
+    color: #0c7ab6;
+    margin-top: 1rem;
+    margin-bottom: -5px;
 }
-
-div:hover:before {
-
-  left: 0;
-
+.card .content h6{
+    font-size: 30px;
+    margin-bottom: 15px;
 }
-
-div:hover:after {
-    
-  left: 50%;
+.card .content p{
+    font-size: 15px;
 }
-
-
-
-
-.div-administrador{
-
-    align-items:center;
-    text-align:center;
-    padding: 25px;
-    margin-bottom: 30px;
-    margin-top: 30px;
-    position: relative;  
-    top: 20%;      
-    left: 45%;
-
+/* Estilos en el titulo princpal*/
+.Titulo__Principal{
+    border: 2px solid white;
+    color:white;
+    text-align: center;
+    text-transform: uppercase;
+    position: relative;
+    display: flex;
+    top: -300px;
+    left: 470px;
+    bottom: 0;
+    right: 0;
+    background-color: #0c7ab6;
 }
-
-.div-usuario{
-    
-    align-items:center;
-    text-align:center;
-    padding: 25px;
-    margin-bottom: 30px;
-    margin-top: 30px;
-    position: relative;  
-    top: 10%;      
-    left: 45%;
-
-}
-
-.section-cerrar-sesion{
-
-    align-items:center;
-    text-align:center;
-    padding: 15px;
-    margin-bottom: 30px;
-    margin-top: 30px;
-    position: absolute;  
-    top: 0%;      
-    left: 90%;
-    box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
-    width: 125px;
-    font: caption;
-    border-radius: 15px;
-    transition: background-color 1s linear;
-
-}
-
-.section-cerrar-sesion:hover{
-   background-color: #ECECEC;
-}
-
-
-
-
 </style>
 
 <?php
-
 session_start();
-
 class usuario{
-
     public function ver(){
-
         include("conexion.php");
-
             $documento = $_SESSION["documento"];
             $sql = "SELECT * FROM permisos WHERE documento = '$documento'";
 
-            echo '<p class="title"> Seleccione su rol</p>';
+            Echo('
+                
+            ');
     
         if (!$result = $db->query($sql)){
 
@@ -155,45 +120,53 @@ class usuario{
             $rrol = stripslashes($row["id_rol"]);
             
             if ($rrol == "1"){
-
                 //echo "<a href= 'vista_administrador.php'>vista administrador <a/><br/>";
+                Echo(" 
+                <a class='Roles'href= 'vista_administrador.php'> 
+                <div class='card'>
+                    
+                    <div class='content'>
+                        <span class='fondo'></span>
+                        
+                        <div class='img'>
+                            <img src='img_IconoAdministrador.jpg'>
+                        </div>
 
-                echo '<div class="div-administrador">';
-                echo '<a href= "vista_administrador.php"> <span class="material-symbols-outlined"> manage_accounts </span> Administrador <a/>';//span administrador
-                echo '</div> ';
+                        <h4>Rol</h4>
+                        <h6>Administrador</h6>
+                        <p>Puede ingresar como administrador de sistema.</p>         
+                    </div>
+                </div>
+                </a>
+                ");
 
                 $_SESSION["admin"] = "1";
             }
     
             if ($rrol == "2"){
-
                 //echo "<br/><a href= 'vista_usuario.php'>vista usuario <a/><br/>";
+                Echo(" 
+                <a class='Roles' href= 'vista_usuario.php'> 
+                <div class='card'>
+                    <div class='content'>
+                        <span class='fondo'></span>
+                        
+                        <div class='img'>
+                            <img src='img_IconoUsuarios.jpg'>
+                        </div>
 
-                echo '<div class="div-usuario">';
-                echo '<a href= "vista_usuario.php"> <span class="material-symbols-outlined"> person </span> Usuario <a/> <br/>';//span usuario
-                echo '</div> ';
-
+                        <h4>Rol</h4>
+                        <h6>Usuario</h6>
+                        <p>Puede ingresar como usuario de sistema.</p>         
+                    </div>
+                </div>
+                </a>
+                ");
                 $_SESSION["usuario"] = "1";
             }
-            
         }
-
-        
-
-        echo "<br/><br/><br/>";
-
-        echo '<section class="section-cerrar-sesion">';
-        echo '<a href= "salir.php"> <span class="material-symbols-outlined"> logout </span> Cerrar sesion<a/>';// span cerrar sesion
-        echo '</section>';
-
-
-
-
     }
 }
-
 $final = new usuario();
-$final->ver();
-
-
+    $final->ver();
 ?>
