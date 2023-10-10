a6690b7cc3a92aa1fbc13d4421c4980eac7b837f
Brayan Andres Hc <118775234+Brayan-Hc11@users.noreply.github.com>
2023-10-10 17:31:06 -0500

    Update vista_usuario.php

--- BlackLabel.V4.0/vista_usuario.php
+++ BlackLabel.V4.0/vista_usuario.php
@@ -1,6 +1,12 @@
 <?php
 include ("seguridad_usuario.php");
-
-echo "Vista usuario";
-
+Echo("
+<h1>Bienvenido a la vista de usuario</h1>
+<section>
+    <a href='salir.php'> 
+        <span class='material-symbols-outlined'>logout</span>
+        Cerrar seción
+    </a>
+</seption>  
+");
 ?>
