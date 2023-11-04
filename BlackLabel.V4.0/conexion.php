4fff5dd3023871a57968184ecc4d3cc1fa1c2e98
Julian Rojas <129390687+judali05@users.noreply.github.com>
2023-11-04 15:41:37 -0500

    Update conexion.php

--- BlackLabel.V4.0/conexion.php
+++ BlackLabel.V4.0/conexion.php
@@ -4,4 +4,4 @@ $db = new mysqli('localhost', 'root', '','blacklabel');
 	$acentos = $db->query("SET NAMES 'utf8'");
 if($db->connect_error > 0){
 	die('Unable to connect to database [' .  $db->connect_error . ']');
-}?> 
+}?>  
