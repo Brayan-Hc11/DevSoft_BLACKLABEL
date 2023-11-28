7e88db2358e413b6aef2e156d472693f87c9be65
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-11-28 13:47:53 -0500

    Add files via upload

---
+++ BLACKLABEL.V05.1-Carrito/PROD/Prod_Laptop6.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		
+		<script src="https://kit.fontawesome.com/b217920474.js" crossorigin="anonymous"></script>
+
+        <title>Categoria:  Laptops</title>
+		
+        <link rel="stylesheet" href="ViewProducts.css" />
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />    
+	</head>
+    <body>
+	
+	<header>
+		<h1 ><a href="index.php" class='title'><span class='material-symbols-outlined'>sell</span>BLACKLABEL</a></h1>
+	</header>
+
+
+		<div class="container-title">Categoria: Laptops</div>
+
+		<main>
+			<div class="container-img">
+				<img src="productos/img_laptop6.png"alt="imagen-producto"/>
+			</div>
+			
+            <div class="container-info-product">
+				<div class="container-price">
+					<span>$ 1.299.000</span><span class="Descuento">12% OFF</span>
+					<i class="fa-solid fa-angle-right"></i>
+				</div>
+
+				<div class="container-details-product">
+					<div class="form-group">
+                    Laptop Hp 14-fq1051la, Ryzen 3 5300u, 12 Gb 256 Gb, 14
+                    </div>
+					
+                    <div class="form-group">
+						<label for="size"></label>
+					
+					</div>
+					<button class="btn-clean"></button>
+				</div>
+
+				<div class="container-add-cart">
+					
+                    <div class="container-quantity">
+						<input type="number"placeholder="1"value="1"min="1"class="input-quantity"/>
+						
+                        <div class="btn-increment-decrement">
+							<i class="fa-solid fa-chevron-up" id="increment"></i>
+							<i class="fa-solid fa-chevron-down" id="decrement"></i>
+						</div>
+					</div>
+					<button class="btn-add-to-cart">
+						<i class="fa-solid fa-plus"></i>
+						Añadir al carrito
+					</button>
+				</div>
+				<div class="container-description">
+					<div class="title-description">
+						<h4>Lo que tienes que saber de este producto</h4>
+						<i class="fa-solid fa-chevron-down"></i>
+					</div>
+					<div class="text-description">
+						<p>
+<ul>
+    <li>Procesador AMD.</li>
+    <li>Conexión wifi.</li>
+    <li>Cuenta con puerto USB y puerto HDMI.</li>
+    <li>La duración de la batería depende del uso que se le dé al producto.</li>
+</ul>
+						</p>
+					</div>
+				</div>
+
+				<div class="container-additional-information">
+					<div class="title-additional-information">
+						<h4>Descripción</h4>
+						<i class="fa-solid fa-chevron-down"></i>
+					</div>
+					<div class="text-additional-information hidden">
+<p>                    
+Con la laptop 2 en 1 HP Laptop 14-fq1051la vas a disfrutar de la combinación única entre portabilidad y usabilidad. Su principal beneficio es su capacidad de transformarse en una tablet sin perder la potencia y el rendimiento de una computadora portátil.
+</p><br>
+Eficiencia a tu alcance
+Su procesador AMD, te permitirá ejecutar programas variados y procesos indispensables para desenvolverte en el día a día, ya sea en tu trabajo o en los momentos de ocio en tu hogar.
+</p><br>
+Potente disco sólido
+El disco sólido de 256 GB hace que el equipo funcione a gran velocidad y por lo tanto te brinda mayor agilidad para operar con diversos programas.
+</p><br>        
+            </div>
+				</div>
+
+				<div class="container-reviews">
+					<div class="title-reviews">
+						<h4>Características del producto</h4>
+						<i class="fa-solid fa-chevron-down"></i>
+					</div>
+					<div class="text-reviews hidden">
+						<p>
+                       
+<ul>
+    <li>Tamaño de la pantalla: 14 "</li>
+    <li>Con pantalla táctil: No</li>
+</ul>
+
+                        </p>
+					</div>
+				</div>
+
+				<div class="container-social">
+					<span>Compartir</span>
+					<div class="container-buttons-social">
+						<a href="#"><i class="fa-solid fa-envelope"></i></a>
+						<a href="#"><i class="fa-brands fa-facebook"></i></a>
+						<a href="#"><i class="fa-brands fa-twitter"></i></a>
+						<a href="#"><i class="fa-brands fa-instagram"></i></a>
+						<a href="#"><i class="fa-brands fa-pinterest"></i></a>
+					</div>
+				</div>
+			</div>
+		</main>
+
+		<section class="container-related-products">
+			<h2>Productos Relacionados</h2>
+			<div class="card-list-products">
+				
+				<div class="card">
+					<div class="card-img">
+						<img src="productos/img_Laptop4.png"alt="producto-1"/>
+					</div>
+					
+                    <div class="info-card">
+						<div class="text-product">
+							<h3><a href="Prod_Laptop4.php">Portátil Asus VivoBook   </a></h3>
+							<p class="category"></p>
+						</div>
+						<div class="price">$ 2.599.000</div></div>
+				</div>
+				
+                <div class="card">
+					
+                    <div class="card-img">
+						<img src="productos/img_Laptop5.png"alt="producto-2"/>
+					</div>
+					
+                    <div class="info-card">
+						<div class="text-product">
+						<h3><a href="Prod_Laptop5.php">Portátil gamer Asus TUF</a></h3>
+							<p class="category"></p>
+						</div>
+						<div class="price">$ 3.299.000</div>
+					</div>
+				</div>
+				
+                
+                <div class="card">
+					<div class="card-img">
+						<img src="productos/img_Laptop7.png"alt="producto-3"/>
+					</div>
+					
+                    <div class="info-card">
+						<div class="text-product">
+						<h3><a href="Prod_Laptop7.php">Portátil Asus VivoBook </a></h3>
+							<p class="category"></p>
+						</div>
+						<div class="price">$ 2.020.553</div>
+					</div>
+				</div>
+				
+                <div class="card">
+					<div class="card-img">
+						<img
+							src="productos/img_Laptop8.png"alt="producto-4"/>
+					</div>
+					
+                    <div class="info-card">
+						<div class="text-product">
+						<h3><a href="Prod_Laptop8.php">Portátil Gamer Acer Nitro</a></h3>
+							<p class="category"></p>
+						</div>
+						<div class="price">$ 2.020.553</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<footer>
+			<p><br><br><br></p>
+		</footer>
+
+		<script src="https://kit.fontawesome.com/81581fb069.js"crossorigin="anonymous"></script>
+		<script src="j-vistaProductos.js"></script>
+	</body>
+</html>
