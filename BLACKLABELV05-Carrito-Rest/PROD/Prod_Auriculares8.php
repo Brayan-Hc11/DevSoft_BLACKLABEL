c7af3b802b11bc7adbddb4c288d0cb16773c91bf
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-12-01 16:32:58 -0500

    Add files via upload

---
+++ BLACKLABELV05-Carrito-Rest/PROD/Prod_Auriculares8.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		
+		<script src="https://kit.fontawesome.com/b217920474.js" crossorigin="anonymous"></script>
+
+        <title>Categoria: Auriculares</title>
+		
+        <link rel="stylesheet" href="ViewProducts.css" />
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />    
+	</head>
+	
+    <body>
+	
+	<header>
+		<h1 ><a href="index.php" class='title'><span class='material-symbols-outlined'>sell</span>BLACKLABEL</a></h1>
+	</header>
+		<div class="container-title">Categoria: Auriculares</div>
+
+		<main>
+			<div class="container-img">
+				<img src="productos/img_Auricular8.png"alt="imagen-producto"/>
+			</div>
+			
+            <div class="container-info-product">
+				<div class="container-price">
+					<span>$ 33.999</span><span></span>
+					<i class="fa-solid fa-angle-right"></i>
+				</div>
+
+				<div class="container-details-product">
+					<div class="form-group">
+                    Audífonos in-ear inalámbricos i7S TWS negro
+                    </div>
+					
+                    <div class="form-group">
+						<label for="size"></label>
+					
+					</div>
+					<button class="btn-clean"></button>
+				</div>
+
+				<div class="container-add-cart">
+					
+                    <div class="container-quantity">
+						<input type="number"placeholder="1"value="1"min="1"class="input-quantity"/>
+						
+                        <div class="btn-increment-decrement">
+							<i class="fa-solid fa-chevron-up" id="increment"></i>
+							<i class="fa-solid fa-chevron-down" id="decrement"></i>
+						</div>
+					</div>
+					<button class="btn-add-to-cart">
+						<i class="fa-solid fa-plus"></i>
+						Añadir al carrito
+					</button>
+				</div>
+				<div class="container-description">
+					<div class="title-description">
+						<h4>Lo que tienes que saber de este producto</h4>
+						<i class="fa-solid fa-chevron-down"></i>
+					</div>
+					<div class="text-description">
+						<p>
+<ul>
+    <li>Cuenta con tecnología True Wireless.</li>
+    <li>Alcance inalámbrico de 10 m.</li>
+    <li>Modo manos libres incluido.</li>
+    <li>Con estuche de carga inalámbrica.</li>
+    <li>Con micrófono incorporado.</li>
+    <li>Sonido superior y sin límites.</li>
+    <li>Cómodos y prácticos.</li>
+    <li>Las imágenes pueden ser ilustrativas.</li>
+</ul>
+						</p>
+					</div>
+				</div>
+
+				<div class="container-additional-information">
+					<div class="title-additional-information">
+						<h4>Descripción</h4>
+						<i class="fa-solid fa-chevron-down"></i>
+					</div>
+					<div class="text-additional-information hidden">
+<p>                    
+En la calle, en el colectivo o en la oficina, ten siempre a mano tus audífonos i7S TWS y ¡escápate de la rutina por un rato! 
+Vas a poder disfrutar de la música que más te gusta y de tus podcasts favoritos cuando quieras y donde quieras.
+</p><br>
+<p>
+El formato perfecto para ti
+Al ser in-ear, mejoran la calidad del audio y son de tamaño pequeño para poder insertarse en tu oreja. Son ideales para 
+acompañarte a la hora de hacer ejercicio mientras te sumerges en el mejor sonido envolvente.
+</p><br>
+<p>
+Wireless al 100%
+A diferencia de otras conectividades, este dispositivo está fabricado con tecnología TWS (True Wireless Stereo). La misma 
+permite una conexión inalámbrica total y funciona sin un solo cable entre pares. Con ella podrás disfrutar del entorno sonoro sin necesidad de estar pendiente de conexiones físicas.
+</p><br>
+
+        </div>
+				</div>
+
+				<div class="container-reviews">
+					<div class="title-reviews">
+						<h4>Características del producto</h4>
+						<i class="fa-solid fa-chevron-down"></i>
+					</div>
+					<div class="text-reviews hidden">
+						<p>
+                       
+<ul>
+    <li>Es inalámbrico: Sí</li>
+    <li>Con micrófono: Sí</li>
+</ul>
+
+                        </p>
+					</div>
+				</div>
+
+				<div class="container-social">
+					<span>Compartir</span>
+					<div class="container-buttons-social">
+						<a href="#"><i class="fa-solid fa-envelope"></i></a>
+						<a href="#"><i class="fa-brands fa-facebook"></i></a>
+						<a href="#"><i class="fa-brands fa-twitter"></i></a>
+						<a href="#"><i class="fa-brands fa-instagram"></i></a>
+						<a href="#"><i class="fa-brands fa-pinterest"></i></a>
+					</div>
+				</div>
+			</div>
+		</main>
+
+		<section class="container-related-products">
+			<h2>Productos Relacionados</h2>
+			<div class="card-list-products">
+				
+				<div class="card">
+					<div class="card-img">
+						<img src="productos/img_Auricular1.png"alt="producto-1"/>
+					</div>
+					
+                    <div class="info-card">
+						<div class="text-product">
+							<h3><a href="Prod_Auriculares1.php">Torre Cpu Gamer Athlon </a></h3>
+							<p class="category"></p>
+						</div>
+						<div class="price">$ 1.282.500</div></div>
+				</div>
+				
+                <div class="card">
+					
+                    <div class="card-img">
+						<img src="productos/img_Auricular2.png"alt="producto-2"/>
+					</div>
+					
+                    <div class="info-card">
+						<div class="text-product">
+						<h3><a href="Prod_Auriculares2.php">Audífonos Logitech</a></h3>
+							<p class="category"></p>
+						</div>
+						<div class="price">$ 579.900</div>
+					</div>
+				</div>
+				
+                
+                <div class="card">
+					<div class="card-img">
+						<img src="productos/img_Auricular3.png"alt="producto-3"/>
+					</div>
+					
+                    <div class="info-card">
+						<div class="text-product">
+						<h3><a href="Prod_Auriculares3.php">Audífonos in-ear gamer</a></h3>
+							<p class="category"></p>
+						</div>
+						<div class="price">$ 359.000</div>
+					</div>
+				</div>
+				
+                <div class="card">
+					<div class="card-img">
+						<img
+							src="productos/img_Auricular4.png"alt="producto-4"/>
+					</div>
+					
+                    <div class="info-card">
+						<div class="text-product">
+						<h3><a href="Prod_Auriculares4.php">Audífonos in-ear gamer</a></h3>
+							<p class="category"></p>
+						</div>
+						<div class="price">$ 359.000</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<footer>
+			<p><br><br><br></p>
+		</footer>
+		<script src="https://kit.fontawesome.com/81581fb069.js"crossorigin="anonymous"></script>
+		<script src="j-vistaProductos.js"></script>
+	</body>
+</html>
