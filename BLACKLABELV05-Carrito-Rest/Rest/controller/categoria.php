7e81000664de0e8b75ac07de6dc0ed20d879d1db
Esteban Salcedo <115185706+Tebancedoo@users.noreply.github.com>
2023-12-01 19:35:54 -0500

    Add files via upload

--- BLACKLABELV05-Carrito-Rest/Rest/controller/categoria.php
+++ BLACKLABELV05-Carrito-Rest/Rest/controller/categoria.php
@@ -3,40 +3,40 @@
 
     require_once("../config/conexion.php");
     require_once("../models/Categoria.php");
-    $categoria = new Categoria();
+    $producto = new Producto();
 
     $body = json_decode(file_get_contents("php://input"), true);
 
     switch($_GET["op"]){
         
         case "GetAll":
-            $datos=$categoria->get_categoria();
+            $datos=$producto->get_producto();
             echo json_encode($datos);
         break;
 
         case "GetId":
-            $datos=$categoria->get_categoria_x_id($body["cat_id"]);
+            $datos=$producto->get_producto_x_id($body["id_producto"]);
             echo json_encode("hola");
         break;
 
         case "Insert":
-            $datos=$categoria->insert_categoria($body["cat_nom"], $body["cat_obs"]);
+            $datos=$producto->insert_producto($body["nombre_producto"], $body["codigo_producto"]);
             echo json_encode("Insert correcto");
         break;
 
         case "Update":
-            $datos=$categoria->update_categoria($body["cat_id"], $body
-            ["cat_nom"], $body["cat_obs"]);
+            $datos=$producto->update_producto($body["id_producto"], $body
+            ["codigo_producto"], $body["nombre_producto"], $body["id_categoria"], $body["documento_creador"], $body["precio_producto"], $body["id_estado"]);
             echo json_encode("Update correcto");
         break;
 
         case "Delete":
-            $datos=$categoria->update_categoria($body["cat_id"]);
+            $datos=$producto->delete_producto($body["id_producto"]);
             echo json_encode("Delete correcto");
         break;
 
         case "Eliminar":
-            $datos=$categoria->update_categoria($body["cat_id"]);
+            $datos=$producto->eliminar_producto($body["id_producto"]);
             echo json_encode("Eliminar correcto");
         break;
     }
--- BLACKLABELV05-Carrito-Rest/Rest/models/Categoria.php
+++ BLACKLABELV05-Carrito-Rest/Rest/models/Categoria.php
@@ -1,71 +1,71 @@
 <?php
-    class Categoria extends Conectar{
-        public function get_categoria(){
+    class Producto extends Conectar{
+        public function get_producto(){
             $conectar= parent::Conexion();
             parent::set_names();
-                $sql = "SELECT * FROM categorias WHERE est=1";
+                $sql = "SELECT * FROM productos WHERE id_estado=1";
                 $sql = $conectar -> prepare($sql);
                 $sql -> execute();
             return $resultado = $sql -> fetchAll(PDO::FETCH_ASSOC);
         }
 
-        public function get_categoria_x_id($cat_id){
+        public function get_producto_x_id($id_producto){
             $conectar = parent::Conexion();
             parent::set_names();
-                $sql = "SELECT * FROM categorias WHERE est=1 AND cat_id = ?";
+                $sql = "SELECT * FROM productos WHERE id_estado=1 AND id_producto = ?";
                 $sql = $conectar -> prepare($sql);
-                $sql -> bindParam(1, $cat_id);
+                $sql -> bindParam(1, $id_producto);
                 $sql -> execute();
             return $resultado = $sql -> fetchAll(PDO::FETCH_ASSOC);
         }
 
-        public function insert_categoria($cat_nom, $cat_obs){
+        public function insert_producto($nombre_producto, $codigo_producto){
             $conectar = parent::Conexion();
             parent::set_names();
-                $sql = "INSERT INTO categorias (cat_id, cat_nom, cat_obs, est) VALUES
-                (NULL,?,?,'1')";
+                $sql = "INSERT INTO productos (id_producto, codigo_producto, nombre_producto, id_categoria, documento_creador, precio_producto, id_estado) VALUES
+                (?,NULL,?,?,?,?,'1')";
                 $sql = $conectar -> prepare($sql);
-                $sql -> bindParam(1, $cat_nom);
-                $sql -> bindParam(2, $cat_obs);
+                $sql -> bindParam(1, $nombre_producto);
+                $sql -> bindParam(2, $codigo_producto);
                 $sql -> execute();
             return $resultado = $sql -> fetchAll(PDO::FETCH_ASSOC);
         }
 
-        public function update_categoria($cat_id, $cat_nom, $cat_obs, $est){
+        public function update_producto($id_producto, $codigo_producto, $nombre_producto, $id_categoria, $documento_creador, $precio_producto, $id_estado){
             $conectar = parent::Conexion();
             parent::set_names();
-                $sql = "UPDATE categorias SET
-                cat_nom = ?,
-                cat_obs = ?
+                $sql = "UPDATE productos SET
+                id_categoria = ?,
+                precio_producto = ?
                 WHERE
-                cat_id = ?";
+                id_producto = ?";
                 $sql = $conectar -> prepare($sql);
-                $sql -> bindParam(1, $cat_nom);
-                $sql -> bindParam(2, $cat_obs);
-                $sql -> bindParam(3, $cat_id);
+                $sql -> bindParam(1, $id_categoria);
+                $sql -> bindParam(2, $precio_producto);
+                $sql -> bindParam(3, $id_producto);
                 $sql -> execute();
             return $resultado = $sql -> fetchAll(PDO::FETCH_ASSOC);
         }
 
-        public function delete_categoria($cat_id){
+        public function delete_producto($id_producto){
             $conectar = parent::Conexion();
             parent::set_names();
-                $sql = "UPDATE categorias SET
-                est = '0'
+                $sql = "UPDATE productos SET
+                id_estado = '2'
                 WHERE
-                cat_id = ?";
+                id_producto = ?";
                 $sql = $conectar -> prepare($sql);
-                $sql -> bindParam(1, $cat_id);
+                $sql -> bindParam(1, $id_producto);
                 $sql -> execute();
             return $resultado = $sql -> fetchAll(PDO::FETCH_ASSOC);
         }
 
-        public function eliminar_categoria($cat_id){
+        public function eliminar_producto($id_producto){
             $conectar = parent::Conexion();
             parent::set_names();
-                $sql = "DELETE FROM  categorias WHERE cat_id = ?";
+                $sql = "DELETE FROM  productos WHERE id_producto = ?";
                 $sql = $conectar -> prepare($sql);
-                $sql -> bindParam(1, $cat_id);
+                $sql -> bindParam(1, $id_producto);
                 $sql -> execute();
             return $resultado = $sql -> fetchAll(PDO::FETCH_ASSOC);
         }
