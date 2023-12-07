8005a03719d18d4b7377140ece8d63eecda15807
Julian Rojas <129390687+judali05@users.noreply.github.com>
2023-12-06 20:22:28 -0500

    Add files via upload

--- BLACKLABELV05-Carrito-Rest/j-carrito.js
+++ BLACKLABELV05-Carrito-Rest/j-carrito.js
@@ -6,118 +6,94 @@ const listaProductos = document.querySelector('#lista-productos');
 let articulosCarrito = [];
 
 cargarEventListeners();
-function cargarEventListeners(){
-    //cuando agregar un producto "presionas el agregar"
+
+function cargarEventListeners() {
+    // Agregar un producto al carrito al hacer clic
     listaProductos.addEventListener('click', agregarProducto);
 
-    //elimina productos  del carrito
+    // Eliminar productos del carrito al hacer clic en "X"
     carrito.addEventListener('click', eliminarProducto);
 
-    //vaciar el carrito
+    // Vaciar todo el carrito
     vaciarCarritoBtn.addEventListener('click', () => {
         articulosCarrito = [];
-
         limpiarHTML();
     });
 }
 
-//Funciones
-function agregarProducto(e){
+// Función para agregar un producto al carrito
+function agregarProducto(e) {
     e.preventDefault();
 
-    if(e.target.classList.contains('agregar-carrito')){ //console.log('Agregando al carrito'); //console.log(e.target.parentElement.parentElement);
+    if (e.target.classList.contains('agregar-carrito')) {
         const productoSeleccionado = e.target.parentElement.parentElement;
         leerDatosProducto(productoSeleccionado);
     }
-
 }
-//Eliminar un producto del carrito 
-function eliminarProducto(e){
-    if(e.target.classList.contains('borrar-producto')){
+
+// Función para eliminar un producto del carrito
+function eliminarProducto(e) {
+    if (e.target.classList.contains('borrar-producto')) {
         const productoId = e.target.getAttribute('data-id');
-
-        articulosCarrito = articulosCarrito.filter( producto => producto.id !== productoId);
-
+        articulosCarrito = articulosCarrito.filter(producto => producto.id !== productoId);
         carritoHTML();
     }
 }
 
-//lee contenido del HTML, al que le dimos click
-function leerDatosProducto(producto){ //console.log(producto);
-
-//crear un objeto con el cotenido del producto actual
+// Función para leer los datos de un producto
+function leerDatosProducto(producto) {
     const infoProducto = {
         imagen: producto.querySelector('img').src,
         titulo: producto.querySelector('h3').textContent,
-        precio: producto.querySelector('.precio span').textContent,
-        id: producto.querySelector('span').getAttribute('data-id'),
-        cantidad: 1
+        precio: parseFloat(producto.querySelector('.price').textContent.replace('$', '')),
+        id: producto.querySelector('.agregar-carrito').getAttribute('data-id'),
+        cantidad: 1,
+    };
+
+    const existe = articulosCarrito.some(producto => producto.id === infoProducto.id);
+
+    if (existe) {
+        const productos = articulosCarrito.map(producto => {
+            if (producto.id === infoProducto.id) {
+                producto.cantidad++;
+            }
+            return producto;
+        });
+        articulosCarrito = [...productos];
+    } else {
+        articulosCarrito = [...articulosCarrito, infoProducto];
     }
 
-//revisa si un elemento ya existe en el carrito 
-const existe = articulosCarrito.some(producto => producto.id === infoProducto.id);
-if(existe){
-    //Actualiza la cantidad
-    const productos = articulosCarrito.map(producto => {
-        if(producto.id === infoProducto.id){
-            producto.cantidad++ ;
-            return producto;
-        }else{
-            return producto;
-        }
-    });
-    articulosCarrito = [...productos]
-}else{
-    //agregamos el producto al carrito
-    //agrega elementos al arreglo de carrito, agregamos elementos al carrito
-    articulosCarrito = [...articulosCarrito, infoProducto];
+    carritoHTML();
 }
 
-//console.log(articulosCarrito);
-
-carritoHTML();
-
-}
-
-//muestra el carrito de compras en el html
+// Función para mostrar el carrito en el HTML
 function carritoHTML() {
-
-    //limpiar html
     limpiarHTML();
-    
-    //Recorre el carrito y genera el HTML
-    articulosCarrito.forEach(producto => {
 
-        const {imagen, titulo, precio, cantidad, id} = producto;
+    articulosCarrito.forEach(producto => {
+        const { imagen, titulo, precio, cantidad, id } = producto;
         const row = document.createElement('tr');
         row.innerHTML = `
         <td> 
-            <img src="${producto.imagen}" width="100">
+            <img src="${imagen}" width="100">
         </td>
-         <td> ${producto.titulo} </td>
-         <td> ${producto.precio}</td>
-         <td> ${producto.cantidad}</td>
+         <td> ${titulo} </td>
+         <td> $${precio.toFixed(2)}</td>
+         <td> ${cantidad}</td>
          <td>
             <a href="#" class="borrar-producto" data-id="${id}">X</a>
          </td>
         `;
 
-        //agrega el html del carrito en el tbody
         contenedorCarrito.appendChild(row);
     });
 }
 
-//elimina los productos del tbody
-function limpiarHTML(){
-    //forma lenta
-    //contenedorCarrito.innerHTML = '';
-
-    while(contenedorCarrito.firstChild){
-        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
-
+// Función para limpiar el HTML del carrito
+function limpiarHTML() {
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
     }
 }
 
-
-/*voy aquiiiiii */
-// agragar funcionalidad carrito al proyecto
